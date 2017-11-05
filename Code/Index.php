dbc174dc7c990a72dbfe91f4a027d7eed1a66614
PepeDuke <32746699+PepeDuke@users.noreply.github.com>
2017-11-05 16:09:13 +0100

    Prihlasovani a odhlasovani

--- Code/Index.php
+++ Code/Index.php
@@ -1,4 +1,7 @@
 ﻿<!DOCTYPE HTML>
+<?php
+    session_start();
+?>
 <html lang="cs">
 	<head>
     <link rel="stylesheet" type="text/css" href="css/mystyle.css">
@@ -7,8 +10,16 @@
 	<ul class="topul">
     	<li><a href="Index.php" class="Title1">LOGOS</a></li>
         <li><a href="Index.php" class="Title2">POLYTECHNIKOS</a></li>
-		<li style="float:right" ><a href="login.php" class="btn">login</a></li>
-		<li style="float:right"><a href="register.php" class="btn">register</a></li>
+          <?php
+		  if (!isset($_SESSION['user'])) {
+			  echo '<li style="float:right" ><a href="login.php" class="btn">login</a></li>';
+			  echo '<li style="float:right"><a href="register.php" class="btn">register</a></li>';
+		  }
+		  else
+		  {
+			  echo '<li style="float:right" ><a href="logout.php" class="btn">' .$_SESSION['user']. '</a></li>';
+		  }
+        ?>
 	</ul>
 	<ul>
 	  <li><a href="Index.php" class="active hav pic"><img border="0" alt="Home" src="images/home-red.png" width="30" height="30"></a></li>
--- Code/login.php
+++ Code/login.php
@@ -4,6 +4,66 @@
     <link rel="stylesheet" type="text/css" href="css/mystyle.css">
 	</head>
 	<body>
+    <?php
+    $name = "";
+	$password = "";
+	$pass = "";
+	$error = "";
+	
+	$con=mysqli_connect("localhost","root","") or die(mysql_error());
+	mysqli_select_db($con,'polytech');
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["user"])) {
+			$error = "incorrect login credentials";
+		}
+		else
+		{
+			$name = test_input($_POST["user"]);
+			
+		}
+		if (empty($_POST["pass"])) {
+			$error = "incorrect login credentials";
+		}
+		else
+		{
+			$password = test_input($_POST["pass"]);
+		}
+	
+	if($error == "")
+		{
+			$sql="SELECT Pass FROM user WHERE Nick='$name'";
+			$result=mysqli_query($con,$sql);
+			$count=mysqli_num_rows($result);
+			$arr = mysqli_fetch_assoc($result);
+			if($count == 1)
+			{
+				if(password_verify($password, $arr["Pass"]))
+				{
+					session_start();
+					$_SESSION['user']= $name;
+					header('Location: index.php');
+				}
+				else
+				{
+					$error = "incorrect login credentials";
+				}
+			}
+			else
+			{
+				$error = "incorrect login credentials";
+			}
+		}
+	}
+	
+	function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+	
+    ?>
 	<ul class="topul">
     	<li><a href="Index.php" class="Title1">LOGOS</a></li>
         <li><a href="Index.php" class="Title2">POLYTECHNIKOS</a></li>
@@ -17,9 +77,10 @@
 		<li style="float:right" ><a href="revize.php" class="hav">Revize <u class="NotifNum">0</u></a></li>
 	</ul>
      <div class="login-card">
-     <form>
-   	    <input type="text" name="user" placeholder="Username">
-    	<input type="password" name="pass" placeholder="Password">
+     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+     	<span class="error"><?php echo $error;?></span>
+   	    <input type="text" name="user" placeholder="Username" value="<?php echo $name;?>">
+    	<input type="password" name="pass" placeholder="Password" value="<?php echo $password;?>">
     	<input type="submit" name="login" class="login login-submit" value="login">
  	 </form>
      <div class="login-help">
